92c43ada914d610cd919d48efd43b2790cdb13b8
Incognito357 <jrh.JackSparrow042@yahoo.com>
2016-10-14 00:01:55 -0400

    Starting networking code from scratch again

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+
+#define HOSTPORT 31415
+#define HOSTADDR "10.3.14.15"
 
 #ifdef MASTER
 
-#define LISTENPORT 31415
+#include <vector>
 
 #else
 
@@ -20,34 +28,13 @@ int main()
 {
     #ifdef MASTER
 
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (fd < 0)
-    {
-        printf("Could not create socket.");
-        return -1;
-    }
-
-    struct sockaddr_in addr
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(LISTENPORT);
-
-    if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0)
-    {
-        printf("Error binding address to socket");
-        return -1;
-    }
-
-    listen(fd,
 
     #else
 
-    Mandelbrot m;
+
 
     #endif
-
-    double *vals = new double[1200 * 900];
 
     while (true)
     {
@@ -57,8 +44,14 @@ int main()
 
         #else
 
-        m.Update(vals);
+
 
         #endif
     }
+
+    #ifdef MASTER
+
+
+
+    #endif
 }
