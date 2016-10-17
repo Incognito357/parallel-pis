1864ed15c4b2ac3464812b3cf3afc53128abecb2
Incognito357 <jrh.JackSparrow042@yahoo.com>
2016-10-17 18:12:27 -0400

    Server locally scoped 'ret' back

--- main.cpp
+++ main.cpp
@@ -314,7 +314,7 @@ int main()
                             break;
                         case MessageType::Text:
                             char* buf = new char[m.len + 1];
-                            ret = read(s, &buf, m.len);
+                            int ret = read(s, &buf, m.len);
                             buf[m.len] = 0;
                             printf("Text event, expected buf size: %d, received: %d\n", m.len, ret);
                             printf("-> %s: \"%s\"\n", inet_ntoa(addr.sin_addr), buf);
