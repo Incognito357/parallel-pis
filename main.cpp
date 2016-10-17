9584b2bd02e1790071186bafbed76ea9e8b93048
Incognito357 <jrh.JackSparrow042@yahoo.com>
2016-10-17 18:23:35 -0400

    Make SendText pointer once again

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int SendText(int s, char* msg)
     reply.len = (int)strlen(msg);
     printf("Sending header: %d\n", (int)sizeof(reply));
     printf("Sending msg: %d\n", reply.len);
-    return send(s, &reply, sizeof(reply), 0) + send(s, msg, reply.len, 0);
+    return send(s, &reply, sizeof(reply), 0) + send(s, &msg, reply.len, 0);
 }
 
 int main()
